54fee8e311a8f871970d381422b06fcdc5e6d1af
Luis <me@luisgarcia.me>
2021-04-18 23:03:42 -0400

    work in progress, incorrect # of args

--- WeightChart/aws_resources/hello_world.py
+++ WeightChart/aws_resources/hello_world.py
@@ -5,6 +5,8 @@
 # session persistence, api calls, and more.
 # This sample is built using the handler classes approach in skill builder.
 import logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
 from ask_sdk_core.skill_builder import SkillBuilder
 from ask_sdk_core.dispatch_components import AbstractRequestHandler
@@ -13,28 +15,29 @@ import ask_sdk_core.utils as ask_utils
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model import Response
 
-from aws_resources.request_handlers.LaunchRequestHandler import LaunchRequestHandler
+#import sys
+#import os
+#print(sys.path)
+#print(os.environ['PYTHONPATH'])
+#from aws_resources.request_handlers.LaunchRequestHandler import LaunchRequestHandler
 
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+class LaunchRequestHandler(AbstractRequestHandler):
+    """Handler for Skill Launch."""
+    def can_handle(self, handler_input):
+        # type: (HandlerInput) -> bool
+        
+        return ask_utils.is_request_type("LaunchRequest")(handler_input)
 
-#class LaunchRequestHandler(AbstractRequestHandler):
-#    """Handler for Skill Launch."""
-#    def can_handle(self, handler_input):
-#        # type: (HandlerInput) -> bool
-#        
-#        return ask_utils.is_request_type("LaunchRequest")(handler_input)
-#
-#    def handle(self, handler_input):
-#        # type: (HandlerInput) -> Response
-#        speak_output = "Welcome, you can say Hello or Help. Which would you like to try?"
-#
-#        return (
-#            handler_input.response_builder
-#                .speak(speak_output)
-#                .ask(speak_output)
-#                .response
-#        )
+    def handle(self, handler_input):
+        # type: (HandlerInput) -> Response
+        speak_output = "Welcome, you can say Hello or Help. Which would you like to try?"
+
+        return (
+            handler_input.response_builder
+                .speak(speak_output)
+                .ask(speak_output)
+                .response
+        )
 
 
 class HelloWorldIntentHandler(AbstractRequestHandler):
