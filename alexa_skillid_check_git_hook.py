03b42560251995e536b2fbe5df3cddf89396b6b2
Luis <me@luisgarcia.me>
2021-04-18 22:01:36 -0400

    Test hook

--- alexa_skillid_check_git_hook.py
+++ alexa_skillid_check_git_hook.py
@@ -8,7 +8,7 @@ for root, directories, files in os.walk(path, topdown=False):
     for name in files:
         if 'serverless.yml' in name:
             file_path = os.path.join(root, name)
-            with(file_obj = open(file_path,'r')):
+            with open(file_path,'r') as file_obj:
                 for line in fileObj.readlines():
                     if 'alexaSkill' in line and 'amzn1.ask.skill' in line
                         print("Don't commit the Alexa Skill Id in the serverless.yml file.")
